781adc6d89f8451c1885bc46b535801a0e1df665
Jon Nyman <nymanjon@gmail.com>
2024-02-09 20:35:47 -0700

    Don't watch when building for prod.

--- tasks/start.ts
+++ tasks/start.ts
@@ -36,44 +36,46 @@ await handleHTML(targetDirectory, mappable)
 await handleSw(targetDirectory)
 console.timeEnd("Initializing")
 
-const srcWatcher = watch(
-    `./src`,
-    { recursive: true },
-    async (_, filename) => {
-        if (filename === "web/file-map.ts") {
-            return
-        }
-        let updateType = await getUpdateType(filename)
-        switch (updateType) {
-            case UpdateType.JS:
-                await buildJS(isProd, targetDirectory)
-                break
-            case UpdateType.CSS:
-                // Get Hex Hash of CSS File and Copy to Public
-                handleCssUpdate(<string>filename, targetDirectory)
-                break
-            case UpdateType.HTML:
-                // Copy HTML File to Public
-                // Just do that once. I can restart the server if I need to.
-                break
-            case UpdateType.JSON:
-                handleJsonUpdate(<string>filename, targetDirectory)
-                break
-            case UpdateType.OTHER:
-                // Do nothing
-        }
+if (build !== "prod") {
+    const srcWatcher = watch(
+        `./src`,
+        { recursive: true },
+        async (_, filename) => {
+            if (filename === "web/file-map.ts") {
+                return
+            }
+            let updateType = await getUpdateType(filename)
+            switch (updateType) {
+                case UpdateType.JS:
+                    await buildJS(isProd, targetDirectory)
+                    break
+                case UpdateType.CSS:
+                    // Get Hex Hash of CSS File and Copy to Public
+                    handleCssUpdate(<string>filename, targetDirectory)
+                    break
+                case UpdateType.HTML:
+                    // Copy HTML File to Public
+                    // Just do that once. I can restart the server if I need to.
+                    break
+                case UpdateType.JSON:
+                    handleJsonUpdate(<string>filename, targetDirectory)
+                    break
+                case UpdateType.OTHER:
+                    // Do nothing
+            }
 
-        await fileMapper(targetDirectory)
-        await handleSw(targetDirectory)
+            await fileMapper(targetDirectory)
+            await handleSw(targetDirectory)
+        }
+    )
+
+    if (build === "dev") {
+        server({ targetDirectory })
     }
-)
 
-if (build === "dev") {
-    server({ targetDirectory })
+    process.on("SIGINT", () => {
+        srcWatcher.close();
+        process.exit(0);
+    });
 }
-
-process.on("SIGINT", () => {
-    srcWatcher.close();
-    process.exit(0);
-});
 
