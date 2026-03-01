2fb915d56d912562261e7b609f2644d8d7fe2543
Jon Nyman <nymanjon@gmail.com>
2026-02-28 22:14:55 -0700

    Code clean up.

--- bin/lib/file-mapper.ts
+++ bin/lib/file-mapper.ts
@@ -4,84 +4,83 @@ import path from "node:path"
 import { getHash, glob, write } from "./system.ts"
 
 export interface FileMapper {
-    url: string
-    file: string
+  url: string
+  file: string
 }
 
 function isFileMapper(x: any | undefined): x is FileMapper {
-    return x?.url && x?.file
+  return x?.url && x?.file
 }
 
 let option: { isRunning: boolean, isWaiting: NodeJS.Timeout | null } = {
-    isRunning: false,
-    isWaiting: null
+  isRunning: false,
+  isWaiting: null
 }
 
 export async function fileMapper(targetDirectory: string, force = false) {
-    if (option.isRunning || option.isWaiting || !force) {
-        if (option.isWaiting) {
-            clearTimeout(option.isWaiting)
-        }
-        option.isWaiting = setTimeout(() => {
-            option.isWaiting = null
-            fileMapper(targetDirectory, true)
-        }, 30)
-        return
+  if (option.isRunning || option.isWaiting || !force) {
+    if (option.isWaiting) {
+      clearTimeout(option.isWaiting)
     }
-    try {
-        option.isRunning = true
-        console.time("File Mapper")
+    option.isWaiting = setTimeout(() => {
+      option.isWaiting = null
+      fileMapper(targetDirectory, true)
+    }, 30)
+    return
+  }
+  try {
+    option.isRunning = true
+    console.time("File Mapper")
 
-        let oldFileMapperFiles = await glob("**/file-map.*.js", targetDirectory)
-        Promise.all(oldFileMapperFiles.map(async x => {
-            await rm(`${targetDirectory}/${x}`)
-        }))
+    let oldFileMapperFiles = await glob("**/file-map.*.js", targetDirectory)
+    Promise.all(oldFileMapperFiles.map(async x => {
+      await rm(`${targetDirectory}/${x}`)
+    }))
 
-        let files: string[] = await glob("**/*.{js,css,json,ico,svg,png}", targetDirectory)
+    let files: string[] = await glob("**/*.{js,css,json,ico,svg,png}", targetDirectory)
 
-        let mapper = files.map(x => {
-            let parsed = path.parse(x)
-            let parsed2 = path.parse(parsed.name)
-            let url = `/${parsed.dir}/${parsed2.name}${parsed.ext}`
-            let file = `/${x}`
-            return {
-                url,
-                file,
-            }
-        })
-        .filter(isFileMapper)
+    let mapper = files.map(x => {
+      let parsed = path.parse(x)
+      let parsed2 = path.parse(parsed.name)
+      let url = `/${parsed.dir}/${parsed2.name}${parsed.ext}`
+      let file = `/${x}`
+      return {
+        url,
+        file,
+      }
+    })
+    .filter(isFileMapper)
 
-        if (option.isWaiting) return
-        // Write mapper to file in src/web/file-map.js
-        let fileMapJsonContent = JSON.stringify(mapper)
-        let hash = getHash(fileMapJsonContent)
-        let fileMapUrl = `/web/file-map.${hash}.js`
+    if (option.isWaiting) return
+    // Write mapper to file in src/web/file-map.js
+    let fileMapJsonContent = JSON.stringify(mapper)
+    let hash = getHash(fileMapJsonContent)
+    let fileMapUrl = `/web/file-map.${hash}.js`
 
-        fileMapJsonContent = `${fileMapJsonContent.slice(0, -1)},{"url":"/web/file-map.js","file":"${fileMapUrl}"}]`
+    fileMapJsonContent = `${fileMapJsonContent.slice(0, -1)},{"url":"/web/file-map.js","file":"${fileMapUrl}"}]`
 
-        let fileMapContent = `(() => { self.app = { links: ${fileMapJsonContent} } })()`
-        await write(`${targetDirectory}${fileMapUrl}`, fileMapContent)
+    let fileMapContent = `(() => { self.app = { links: ${fileMapJsonContent} } })()`
+    await write(`${targetDirectory}${fileMapUrl}`, fileMapContent)
 
-        let globalFiles =
-            mapper
-            .filter(x => x.url.includes(".global."))
-            .map(x => x.file)
-            .join(`","`)
+    let globalFiles =
+      mapper
+      .filter(x => x.url.includes(".global."))
+      .map(x => x.file)
+      .join(`","`)
 
-        let swFile = mapper.find(x => x.url === "/web/sw.js")?.file
+    let swFile = mapper.find(x => x.url === "/web/sw.js")?.file
 
-        let globals = `${globalFiles && `"`}${globalFiles}${globalFiles && `",`}`
-        // Create service worker central file
-        if (option.isWaiting) return
-        await write(
-            `${targetDirectory}/web/sw.js`,
-            `importScripts("${fileMapUrl}",${globals}"${swFile}")`)
+    let globals = `${globalFiles && `"`}${globalFiles}${globalFiles && `",`}`
+    // Create service worker central file
+    if (option.isWaiting) return
+    await write(
+      `${targetDirectory}/web/sw.js`,
+      `importScripts("${fileMapUrl}",${globals}"${swFile}")`)
 
-    } catch (error) {
-        console.error(error)
-    } finally {
-        option.isRunning = false
-        console.timeEnd("File Mapper")
-    }
+  } catch (error) {
+    console.error(error)
+  } finally {
+    option.isRunning = false
+    console.timeEnd("File Mapper")
+  }
 }
-
